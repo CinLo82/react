434404fb35ea4332217355517d540f06a27db3b3
Cintia Losada <cinlos3382@gmail.com>
2025-01-09 15:02:57 -0300

    agrega accesibilidad al botón de reinicio y pruebas para incrementar, decrementar y reiniciar en CounterApp

--- counter-app/tests/CounterApp.test.jsx
+++ counter-app/tests/CounterApp.test.jsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { CounterApp } from '../src/CounterApp'
 
 
 describe('Pruebas en <CounterApp />', () => {
+    const initialValue = 10
 
     test('Debe hacer match con snapshot', () => {
         const { container } = render( <CounterApp /> )
@@ -16,9 +17,33 @@ describe('Pruebas en <CounterApp />', () => {
 
         render ( <CounterApp value={100} /> )
         expect(screen.getByText('100')).toBeTruthy()
-        expect(screen.getByRole('heading', {level: 2}).innerHTML).toBe('100')
+        // expect(screen.getByRole('heading', {level: 2}).innerHTML).toBe('100')
     })
 
+    test('Debe de incrementar con el boton +1 ', () => {
+        render ( <CounterApp value={initialValue} /> )
+        fireEvent.click(screen.getByText('+1'))
+        expect(screen.getByText(initialValue + 1)).toBeTruthy()
+    })
 
+    test('Debe de decrementar con el boton -1 ', () => {
+        render ( <CounterApp value={initialValue} /> )
+        fireEvent.click(screen.getByText('-1'))
+        screen.debug()
+        expect(screen.getByText(initialValue - 1)).toBeTruthy()
+    })
+
+    test('Debe de colocar el valor por defecto con el btn reset ', () => {
+        render ( <CounterApp value={initialValue} /> )
+        fireEvent.click(screen.getByText('-1'))
+        fireEvent.click(screen.getByText('-1'))
+        fireEvent.click(screen.getByText('-1'))
+        // fireEvent.click(screen.getByText('Reset'))
+
+        fireEvent.click(screen.getByRole('button', {name: 'btn-reset'}))
+
+        expect(screen.getByText(initialValue)).toBeTruthy()
+    })
+    
 })
 
